b9fa71b4b4439700e327eb12b0e4958097498e9a
asakaw <asakaw@53ea604a-054e-0410-8546-456bafaea6b0>
2010-02-21 13:48:35 +0000

    update addmanifest bat file
    git-svn-id: http://dokan.googlecode.com/svn/trunk@91 53ea604a-054e-0410-8546-456bafaea6b0

--- dokan_control/addmanifest.bat
+++ dokan_control/addmanifest.bat
@@ -1,6 +1,6 @@
 
+SET MT_PATH="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin\mt.exe"
+%MT_PATH% -manifest "dokanctl.exe.manifest"  -outputresource:"objchk_wlh_x86\i386\dokanctl.exe"
+%MT_PATH% -manifest "dokanctl.exe.manifest"  -outputresource:"objchk_wnet_x86\i386\dokanctl.exe"
+%MT_PATH% -manifest "dokanctl.exe.manifest"  -outputresource:"objchk_win7_x86\i386\dokanctl.exe"
 
-:: "%VS80COMNTOOLS%..\..\SDK\v2.0\bin\mt.exe" -manifest "dokanctl.exe.manifest"  -outputresource:"objchk_wlh_x86\i386\dokanctl.exe"
-:: "%VS80COMNTOOLS%..\..\SDK\v2.0\bin\mt.exe" -manifest "dokanctl.exe.manifest"  -outputresource:"objchk_wnet_x86\i386\dokanctl.exe"
-
-"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin\mt.exe" -manifest "dokanctl.exe.manifest"  -outputresource:"objchk_wlh_amd64\amd64\dokanctl.exe"
