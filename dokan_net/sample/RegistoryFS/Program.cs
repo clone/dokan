ec7d31d6f4ef22b0d636edbcc96c4c6fbd3c02d3
asakaw <asakaw@53ea604a-054e-0410-8546-456bafaea6b0>
2010-07-17 03:29:46 +0000

    change RegistoryFS sample to work with Dokan 0.6.0
    git-svn-id: http://dokan.googlecode.com/svn/trunk@162 53ea604a-054e-0410-8546-456bafaea6b0

--- dokan_net/sample/RegistoryFS/Program.cs
+++ dokan_net/sample/RegistoryFS/Program.cs
@@ -263,11 +263,36 @@ namespace RegistoryFS
         static void Main(string[] args)
         {
             DokanOptions opt = new DokanOptions();
-            opt.DriveLetter = 'r';
+            opt.MountPoint = "r:\\";
             opt.DebugMode = true;
             opt.UseStdErr = true;
             opt.VolumeLabel = "RFS";
-            DokanNet.DokanMain(opt, new RFS());
+            int status = DokanNet.DokanMain(opt, new RFS());
+            switch (status)
+            {
+                case DokanNet.DOKAN_DRIVE_LETTER_ERROR:
+                    Console.WriteLine("Drvie letter error");
+                    break;
+                case DokanNet.DOKAN_DRIVER_INSTALL_ERROR:
+                    Console.WriteLine("Driver install error");
+                    break;
+                case DokanNet.DOKAN_MOUNT_ERROR:
+                    Console.WriteLine("Mount error");
+                    break;
+                case DokanNet.DOKAN_START_ERROR:
+                    Console.WriteLine("Start error");
+                    break;
+                case DokanNet.DOKAN_ERROR:
+                    Console.WriteLine("Unknown error");
+                    break;
+                case DokanNet.DOKAN_SUCCESS:
+                    Console.WriteLine("Success");
+                    break;
+                default:
+                    Console.WriteLine("Unknown status: %d", status);
+                    break;
+
+            }
         }
     }
 }
